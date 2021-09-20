54987ef3d7665cc0670f19fc19a4a3bee5c46484
halco102 <ado.halilovic@outlook.com>
2021-09-21 00:49:54 +0200

    exception handling

---
+++ src/main/java/com/diplomski_rad/videoteka/exception/ConflictException.java
@@ -0,0 +1,9 @@
+package com.diplomski_rad.videoteka.exception;
+public class ConflictException extends RuntimeException {
+
+	private static final long serialVersionUID = 1294634607348123805L;
+
+	public ConflictException(final String message) {
+        super(message);
+    }
+}
