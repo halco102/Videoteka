42556c9fa1ecbcac395aceb7ac830823f3e96199
halco102 <ado.halilovic@outlook.com>
2021-10-26 17:14:34 +0200

    update docker-compose and script for cb

--- couchbase-docker/configure.sh
+++ couchbase-docker/configure.sh
@@ -12,23 +12,26 @@ echo $PORT
 
 echo end
 
-sleep 60
+sleep 40
 
 curl -v -X POST $IPADDRESS:8091/pools/default -d memoryQuota=512 -d indexMemoryQuota=512
 
 echo "First command"
-sleep 30
+#sleep 15
 
 curl -v $IPADDRESS:8091/node/controller/setupServices -d services=kv%2cn1ql%2Cindex
 echo "Second command"
-sleep 30
+#sleep 15
 
 
 curl -v $IPADDRESS:8091/settings/web -d port=$PORT -d username=$COUCHBASE_ADMINISTRATOR_USERNAME -d password=$COUCHBASE_ADMINISTRATOR_PASSWORD
 echo "Set password and username"
-sleep 30
+#sleep 15
 
 curl -i -u $COUCHBASE_ADMINISTRATOR_USERNAME:$COUCHBASE_ADMINISTRATOR_PASSWORD -X POST $IPADDRESS:8091/settings/indexes -d 'storageMode=memory_optimized'
+
+
+#sleep 15
 
 couchbase-cli bucket-create -c $IPADDRESS:8091 -u $COUCHBASE_ADMINISTRATOR_USERNAME -p $COUCHBASE_ADMINISTRATOR_PASSWORD \
        --bucket=$COUCHBASE_BUCKET \
@@ -37,12 +40,11 @@ couchbase-cli bucket-create -c $IPADDRESS:8091 -u $COUCHBASE_ADMINISTRATOR_USERN
        --enable-flush=1 \
        --enable-index-replica=1
 
-sleep 30
+#sleep 15
 
 curl -X POST -v -u $COUCHBASE_ADMINISTRATOR_USERNAME:$COUCHBASE_ADMINISTRATOR_PASSWORD http://$IPADDRESS:8093/query/service -d statement=CREATE%20PRIMARY%20INDEX%20primary_index%20ON%20$COUCHBASE_BUCKET
 
 echo $COUCHBASE_BUCKET
-sleep 10
 
 fg 1
 
