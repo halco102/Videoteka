4bb788a52537d4c98e02636309575d3305d0bd54
halco102 <ado.halilovic@outlook.com>
2022-06-15 10:54:35 +0200

    adding repository annotation

--- src/main/java/com/diplomski_rad/videoteka/repository/GenreRepository.java
+++ src/main/java/com/diplomski_rad/videoteka/repository/GenreRepository.java
@@ -3,9 +3,11 @@ package com.diplomski_rad.videoteka.repository;
 import com.diplomski_rad.videoteka.model.Genre;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
 import org.springframework.data.couchbase.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface GenreRepository extends CouchbaseRepository<Genre, String> {
 
     @Query("#{#n1ql.selectEntity} where name = $1 and #{#n1ql.filter}")
