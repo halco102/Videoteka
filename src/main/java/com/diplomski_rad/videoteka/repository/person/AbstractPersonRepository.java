6011a9f5277f9d4b30b830c0e14400b08946af69
halco102 <ado.halilovic@outlook.com>
2022-06-15 10:55:40 +0200

    adding annotation NoRepositoryBean

--- src/main/java/com/diplomski_rad/videoteka/repository/person/AbstractPersonRepository.java
+++ src/main/java/com/diplomski_rad/videoteka/repository/person/AbstractPersonRepository.java
@@ -2,6 +2,8 @@ package com.diplomski_rad.videoteka.repository.person;
 
 import com.diplomski_rad.videoteka.model.Person;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface AbstractPersonRepository<T extends Person> extends CouchbaseRepository<T, String> {
 }
